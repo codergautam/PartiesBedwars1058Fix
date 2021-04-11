94e1f998342df6a81cbef5d5967ccbe0f8444fa5
Coder Gautam <gautamgxtv@gmail.com>
2021-04-11 12:40:23 -0500

    prevents players joining if not party leader

--- src/main/java/com/codergautamyt/bedwars1058partiesfix/events/OnJoinArena.java
+++ src/main/java/com/codergautamyt/bedwars1058partiesfix/events/OnJoinArena.java
@@ -4,6 +4,7 @@ import com.alessiodp.parties.api.Parties;
 import com.alessiodp.parties.api.interfaces.PartiesAPI;
 import com.alessiodp.parties.api.interfaces.PartyPlayer;
 import com.andrei1058.bedwars.api.events.player.PlayerJoinArenaEvent;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -19,6 +20,18 @@ public class OnJoinArena implements Listener {
         //player who tried to join
         Player player = e.getPlayer();
         PartiesAPI api = Parties.getApi();
+        PartyPlayer partyPlayer = api.getPartyPlayer(player.getUniqueId());
 
+        //check if player is in party
+        if(partyPlayer.isInParty()) {
+            //check if player is the leader
+            if(partyPlayer.getRank() == 20) {
+                //transfer all players into leaders game
+            } else {
+                //not leader, cancel event
+                e.setCancelled(true);
+                player.sendMessage(ChatColor.RED+"Only the party leader can warp you into bedwars");
+            }
+        }
     }
 }
