57d438ded7171280512615a8442589f73f7cc8d6
Coder Gautam <gautamgxtv@gmail.com>
2021-04-12 17:17:51 -0500

    feat: warp party members into leaders game

---
+++ README.md
@@ -0,0 +1,3 @@
+### Fixes Parties support for the Bedwars1058 plugin
+
+> This plugin is made specifically for Gamerhome MC, so if it doesnt work I wont be providing support.
--- src/main/java/com/codergautamyt/bedwars1058partiesfix/events/OnJoinArena.java
+++ src/main/java/com/codergautamyt/bedwars1058partiesfix/events/OnJoinArena.java
@@ -2,9 +2,14 @@ package com.codergautamyt.bedwars1058partiesfix.events;
 
 import com.alessiodp.parties.api.Parties;
 import com.alessiodp.parties.api.interfaces.PartiesAPI;
+import com.alessiodp.parties.api.interfaces.Party;
 import com.alessiodp.parties.api.interfaces.PartyPlayer;
+import com.andrei1058.bedwars.api.BedWars;
+import com.andrei1058.bedwars.api.arena.IArena;
 import com.andrei1058.bedwars.api.events.player.PlayerJoinArenaEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -21,14 +26,44 @@ public class OnJoinArena implements Listener {
         Player player = e.getPlayer();
         PartiesAPI api = Parties.getApi();
         PartyPlayer partyPlayer = api.getPartyPlayer(player.getUniqueId());
+        IArena arena = e.getArena();
 
         //check if player is in party
         if(partyPlayer.isInParty()) {
+            Party party = api.getParty(partyPlayer.getPartyId());
             //check if player is the leader
             if(partyPlayer.getRank() == 20) {
                 //transfer all players into leaders game
+                //if spectator, add them as a spectator
+                if(e.isSpectator()) {
+                    //loop through players in party that are not the leader....
+                    for (PartyPlayer pp : party.getOnlineMembers()) {
+                        if (!partyPlayer.getPlayerUUID().equals(pp.getPlayerUUID())) {
+                            //add spectators
+                            arena.addSpectator(Bukkit.getPlayer(pp.getPlayerUUID()),false, arena.getSpectatorLocation());
+                        }
+                    }
+                } else {
+                    //if not spectator, add as player
+                    //loop through players in party that are not the leader....
+                    for (PartyPlayer pp : party.getOnlineMembers()) {
+                        if (!partyPlayer.getPlayerUUID().equals(pp.getPlayerUUID())) {
+                            //add players
+                            arena.addPlayer(Bukkit.getPlayer(pp.getPlayerUUID()), true);
+                        }
+                    }
+                }
             } else {
-                //not leader, cancel event
+                //we check if the player is going to the leaders game
+                //if they are, then we can allow them
+                //if there going to different game, we cancel the event and stop them
+                for (Player playr : arena.getPlayers()) {
+                    if(playr.getUniqueId().equals(party.getLeader())) {
+                        //they ARE going to the leaders game
+                        return;
+                    }
+                }
+                //not going to leaders game, cancel event
                 e.setCancelled(true);
                 player.sendMessage(ChatColor.RED+"Only the party leader can warp you into bedwars");
             }
