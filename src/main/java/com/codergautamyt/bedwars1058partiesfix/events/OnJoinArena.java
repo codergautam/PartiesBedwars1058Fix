9a6a650fa9506dd608c82c7698485745f00f78f0
Coder Gautam <gautamgxtv@gmail.com>
2021-04-16 07:43:14 -0500

    dev build 1

--- src/main/java/com/codergautamyt/bedwars1058partiesfix/events/OnJoinArena.java
+++ src/main/java/com/codergautamyt/bedwars1058partiesfix/events/OnJoinArena.java
@@ -26,54 +26,59 @@ public class OnJoinArena implements Listener {
         PartiesAPI api = Parties.getApi();
         PartyPlayer partyPlayer = api.getPartyPlayer(player.getUniqueId());
         IArena arena = e.getArena();
-        Bukkit.getServer().getLogger().info(player.getName()+" joined arena");
+        //Bukkit.getServer().getLogger().info(player.getName()+" joined arena");
         //check if player is in party
         if(partyPlayer.isInParty()) {
-            Bukkit.getServer().getLogger().info(player.getName()+" in a party");
+            //Bukkit.getServer().getLogger().info(player.getName()+" in a party");
             Party party = api.getParty(partyPlayer.getPartyId());
             //check if player is the leader
             if(partyPlayer.getRank() == 20) {
-                Bukkit.getServer().getLogger().info(player.getName()+" is leader");
+                //Bukkit.getServer().getLogger().info(player.getName()+" is leader");
                 //transfer all players into leaders game
                 //if spectator, add them as a spectator
                 if(e.isSpectator()) {
-                    Bukkit.getServer().getLogger().info(player.getName()+" joined as spectator");
+                    arena.addSpectator(player, false, arena.getSpectatorLocation());
+                    //Bukkit.getServer().getLogger().info(player.getName()+" joined as spectator");
                     //loop through players in party that are not the leader....
+
                     for (PartyPlayer pp : party.getOnlineMembers()) {
                         if (!partyPlayer.getPlayerUUID().equals(pp.getPlayerUUID())) {
                             //add spectators
-                            Bukkit.getServer().getLogger().info(pp.getName()+" joined arena as spec");
+                            //Bukkit.getServer().getLogger().info(pp.getName()+" joined arena as spec");
                             arena.addSpectator(Bukkit.getPlayer(pp.getPlayerUUID()),false, arena.getSpectatorLocation());
                         }
                     }
+                    e.setCancelled(true);
                 } else {
-                    Bukkit.getServer().getLogger().info(player.getName()+" joined as player");
+                    arena.addPlayer(player, true);
+                    //Bukkit.getServer().getLogger().info(player.getName()+" joined as player");
                     //if not spectator, add as player
                     //loop through players in party that are not the leader....
                     for (PartyPlayer pp : party.getOnlineMembers()) {
                         if (!partyPlayer.getPlayerUUID().equals(pp.getPlayerUUID())) {
                             //add players
                             arena.addPlayer(Bukkit.getPlayer(pp.getPlayerUUID()), true);
-                            Bukkit.getServer().getLogger().info(player.getName()+" joined as player");
+                            //Bukkit.getServer().getLogger().info(player.getName()+" joined as player");
                         }
                     }
+                    e.setCancelled(true);
                 }
-            }/* else {
+            } else {
                 //we check if the player is going to the leaders game
                 //if they are, then we can allow them
                 //if there going to different game, we cancel the event and stop them
                 for (Player playr : arena.getPlayers()) {
                     if(playr.getUniqueId().equals(party.getLeader())) {
                         //they ARE going to the leaders game
-                        Bukkit.getServer().getLogger().info(player.getName()+" going to leaders game");
+                        //Bukkit.getServer().getLogger().info(player.getName()+" going to leaders game");
                         return;
                     }
                 }
                 //not going to leaders game, cancel event
-                Bukkit.getServer().getLogger().info(player.getName()+" not going to leaders arena");
+                //Bukkit.getServer().getLogger().info(player.getName()+" not going to leaders arena");
                 player.sendMessage(ChatColor.RED+"Only the party leader can warp you into bedwars");
                 e.setCancelled(true);
-            }*/
+            }
 
         }
     }
